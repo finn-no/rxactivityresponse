ecf82cb39d78597293c77ec54effd0d88113b7b8
Anders Aagaard <aagaande@gmail.com>
2015-10-20 10:03:50 +0200

    example for google login

--- playservices/src/main/java/no/finntech/android/rx/PlayServicesPermissionsConnectionOperator.java
+++ playservices/src/main/java/no/finntech/android/rx/PlayServicesPermissionsConnectionOperator.java
@@ -1,12 +1,14 @@
 package no.finntech.android.rx;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action0;
@@ -17,12 +19,22 @@ public class PlayServicesPermissionsConnectionOperator implements Observable.Ope
     // Exists to allow consumers of play services to unregister from events before we disconnect.
     private static Handler unsubscribeHandler = new Handler();
 
-    private final Context context;
+    private final Activity activity;
+    private final Scope[] scopes;
+    private final RxActivityResponseDelegate.RxResponseHandler responseHandler;
     private final Api<? extends Api.ApiOptions.NotRequiredOptions>[] services;
     private GoogleApiClient client;
 
-    public PlayServicesPermissionsConnectionOperator(Context context, Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
-        this.context = context;
+    @SafeVarargs
+    public PlayServicesPermissionsConnectionOperator(Activity activity, RxActivityResponseDelegate.RxResponseHandler responseHandler, Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
+        this(activity, responseHandler, null, services);
+    }
+
+    @SafeVarargs
+    public PlayServicesPermissionsConnectionOperator(Activity activity, RxActivityResponseDelegate.RxResponseHandler responseHandler, Scope[] scopes, Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
+        this.activity = activity;
+        this.scopes = scopes;
+        this.responseHandler = responseHandler;
         this.services = services;
     }
 
@@ -42,9 +54,14 @@ public class PlayServicesPermissionsConnectionOperator implements Observable.Ope
             @Override
             public void onNext(Boolean permissionGranted) {
                 if (permissionGranted) {
-                    GoogleApiClient.Builder builder = new GoogleApiClient.Builder(context);
+                    GoogleApiClient.Builder builder = new GoogleApiClient.Builder(activity);
                     for (Api<? extends Api.ApiOptions.NotRequiredOptions> service : services) {
                         builder.addApi(service);
+                    }
+                    if (scopes != null) {
+                        for (Scope scope : scopes) {
+                            builder.addScope(scope);
+                        }
                     }
                     builder.addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
                         @Override
@@ -66,7 +83,19 @@ public class PlayServicesPermissionsConnectionOperator implements Observable.Ope
                     builder.addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
                         @Override
                         public void onConnectionFailed(ConnectionResult connectionResult) {
-                            subscriber.onError(new GoogleApiConnectionFailed(connectionResult));
+                            if (connectionResult.hasResolution()) {
+                                RxActivityResponseDelegate rxActivityResponseDelegate = RxActivityResponseDelegate.get(activity);
+                                if (!rxActivityResponseDelegate.hasActiveResponse()) {
+                                    rxActivityResponseDelegate.setResponse(responseHandler);
+                                    try {
+                                        connectionResult.startResolutionForResult(activity, rxActivityResponseDelegate.getRequestCode());
+                                    } catch (IntentSender.SendIntentException e) {
+                                        subscriber.onError(new GoogleApiConnectionFailed(e));
+                                    }
+                                }
+                            } else {
+                                subscriber.onError(new GoogleApiConnectionFailed(connectionResult));
+                            }
                         }
                     });
                     client = builder.build();
--- playservices/src/main/java/no/finntech/android/rx/RxPlayServices.java
+++ playservices/src/main/java/no/finntech/android/rx/RxPlayServices.java
@@ -4,21 +4,41 @@ import java.io.IOException;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.IntentSender;
 import android.location.Location;
 
 import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import rx.Observable;
 
 public class RxPlayServices {
-    public static Observable<GoogleApiClient> getPlayServices(final Context context, final Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
-        return Observable.just(true).lift(new PlayServicesPermissionsConnectionOperator(context, services));
+
+    @SafeVarargs
+    public static Observable<GoogleApiClient> getPlayServices(final Activity activity, final RxActivityResponseDelegate.RxResponseHandler responseHandler, final Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
+        return getPlayServices(activity, responseHandler, null, null, services);
     }
+
+    @SafeVarargs
+    public static Observable<GoogleApiClient> getPlayServices(final Activity activity, final RxActivityResponseDelegate.RxResponseHandler responseHandler, String[] permissions, final Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
+        return getPlayServices(activity, responseHandler, permissions, null, services);
+    }
+
+    @SafeVarargs
+    public static Observable<GoogleApiClient> getPlayServices(final Activity activity, final RxActivityResponseDelegate.RxResponseHandler responseHandler, String[] permissions, Scope[] scopes, final Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
+        return RxPermission.getPermission(activity, responseHandler, permissions)
+                .lift(new PlayServicesPermissionsConnectionOperator(activity, responseHandler, scopes, services));
+    }
+
+    @SafeVarargs
+    public static Observable<GoogleApiClient> getPlayServices(final Activity activity, final RxActivityResponseDelegate.RxResponseHandler responseHandler, Scope[] scopes, final Api<? extends Api.ApiOptions.NotRequiredOptions>... services) {
+        return Observable.just(true)
+                .lift(new PlayServicesPermissionsConnectionOperator(activity, responseHandler, scopes, services));
+    }
+
 
     public static Observable<Location> getLocation(final Activity activity, final LocationRequest locationRequest, final RxActivityResponseDelegate.RxResponseHandler responseHandler) {
         return getLocation(activity, null, locationRequest, responseHandler);
@@ -27,7 +47,7 @@ public class RxPlayServices {
     public static Observable<Location> getLocation(final Activity activity, RxPermissionRationale rationaleOperator, final LocationRequest locationRequest, final RxActivityResponseDelegate.RxResponseHandler responseHandler) {
         String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
         return RxPermission.getPermission(activity, responseHandler, rationaleOperator, permissions)
-                .lift(new PlayServicesPermissionsConnectionOperator(activity, LocationServices.API))
+                .lift(new PlayServicesPermissionsConnectionOperator(activity, responseHandler, LocationServices.API))
                 .lift(new LocationSettingOperator(activity, locationRequest, responseHandler))
                 .lift(new LocationOperator(locationRequest));
     }
